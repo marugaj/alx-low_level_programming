9bee1526dc5b903d75018614bb266f9df0961ac4
marugaj <wanjamaruga@gmail.com>
2023-07-13 09:52:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - This is a command that will print a line using the printf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
