7bff07e6fa1f5c2201c3dc7f97da146f74fecb04
marugaj <wanjamaruga@gmail.com>
2023-07-14 14:42:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		putchar(b + '0');
+		if (b < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
