5fff92b48d5d1e0fbbf1c260704e4b85bedc6a30
marugaj <wanjamaruga@gmail.com>
2023-07-14 13:51:28 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: If 0 (Success)
+*/
+
+int main(void)
+{
+	int a;
+
+	for
+		(a = '0'; a < '10'; a++)
+	{
+		putchar(a + '0');
+	}
+
+	putchar("\n");
+	return (0);
+}
