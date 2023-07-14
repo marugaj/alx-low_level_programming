f2a9eb100304a43c4656d9c983100c801871f818
marugaj <wanjamaruga@gmail.com>
2023-07-14 13:34:15 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: If 0 (Success)
+*/
+
+int main(void)
+
+	int g;
+
+	for (g = 0; g < 10; g++)
+		printf("%d", g);
+	putchar('\n');
+	return (0);
+}
