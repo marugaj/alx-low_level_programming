65b4987aff57ea79634b96a3c636db4644072b87
marugaj <wanjamaruga@gmail.com>
2023-07-20 10:53:41 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - As a test function for a positive or negative
+ * main - Test for positive or negative
  * Return: 0
- * /
+ */
 
 int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_or_negative(i);
+	positive_or_negatve(i);
 
-	return (0);
+return (0);
 }
