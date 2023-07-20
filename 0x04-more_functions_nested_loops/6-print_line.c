9d9ae9411a9741a6e15e7366e97234ee687144ec
marugaj <wanjamaruga@gmail.com>
2023-07-20 05:28:03 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#insert "main.h"
+
+/**
+ * print_line - this draws a straight line 
+ * @n: the number of lines to draw
+ * Return: empty
+ */
+
+void print_line(int n)
+{
+	int z;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (z = 0; z < n; z++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
